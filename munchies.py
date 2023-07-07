072796da15b36e39ac1a5a27a4a09f3036abbc3f
NehaS101 <solankineha297@gmail.com>
2023-07-08 00:09:07 +0530

    mumbai munchies done

--- munchies.py
+++ munchies.py
@@ -8,7 +8,7 @@ class Snack:
 class Canteen:
     def __init__(self):
         self.inventory =[]
-        self.sales.records=[]
+        self.sales_records=[]
 
     def add(self,snack):
         self.inventory.append(snack)
@@ -52,3 +52,54 @@ class Canteen:
         for snack in self.sales_records:
             print(f"ID: {snack.snack_id}\tName: {snack.name}\tPrice: {snack.price}")
 
+def main():
+    canteen = Canteen()
+
+    while True:
+        print("\nCanteen Management System")
+        print("1. Add Snack")
+        print("2. Remove Snack")
+        print("3. Update Snack Availability")
+        print("4. Record Sale")
+        print("5. Display Inventory")
+        print("6. Display Sales Records")
+        print("7. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            snack_id = input("Enter snack ID: ")
+            name = input("Enter snack name: ")
+            price = float(input("Enter snack price: "))
+            available = input("Is the snack available? (yes/no): ").lower() == "yes"
+            snack = Snack(snack_id, name, price, available)
+            canteen.add(snack)
+            print("Snack added to inventory.")
+
+        elif choice == "2":
+            snack_id = input("Enter snack ID: ")
+            canteen.remove(snack_id)
+
+        elif choice == "3":
+            snack_id = input("Enter snack ID: ")
+            available = input("Is the snack available? (yes/no): ").lower() == "yes"
+            canteen.update(snack_id, available)
+
+        elif choice == "4":
+            snack_id = input("Enter snack ID: ")
+            canteen.recorder(snack_id)
+
+        elif choice == "5":
+            canteen.display()
+
+        elif choice == "6":
+            canteen.display_Sales()
+
+        elif choice == "7":
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
