789e63d8cbaf0c72d5a4f3ef817b95d5c03cbe20
NehaS101 <solankineha297@gmail.com>
2023-07-07 12:34:17 +0530

    start working on mumbai munchies

---
+++ munchies.py
@@ -0,0 +1,6 @@
+class Snack:
+    def __init__(self,snack_id,name,price,available):
+        self.snack_id = snack_id
+        self.name = name
+        self.price = price
+        self.available = available
