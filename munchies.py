fa4334dec3bf67605c6a7f0961c5d34ff182c543
NehaS101 <solankineha297@gmail.com>
2023-07-07 22:52:42 +0530

    adding, removing and updating done

--- munchies.py
+++ munchies.py
@@ -4,3 +4,24 @@ class Snack:
         self.name = name
         self.price = price
         self.available = available
+
+class Canteen:
+    def __init__(self):
+        self.inventory =[]
+        self.sales.records=[]
+
+    def add(self,snack):
+        self.inventory.append(snack)
+
+    def remove(self,snack_id):
+        for snack in self.inventory:
+            if snack.snack_id == snack.id:
+                self.inventory.remove(snack)
+                break
+            else:
+                print("Snack not found") 
+
+    def update(self,snack_id,available):
+        for snack in self.inventory:              
+
+                    
